2e5431f22efc0207965fa21fe900948efbee6d75
JasmitMahajan <43086834+JasmitMahajan@users.noreply.github.com>
2021-09-25 21:20:10 -0700

    Updated codes for Homework 2
    Homework 2 Codes

--- decision_tree.py
+++ decision_tree.py
@@ -1,58 +1,107 @@
 #-------------------------------------------------------------------------
 # AUTHOR: Jasmit Mahajan
 # FILENAME: decision_tree.py
-# SPECIFICATION: This program will read the contact_lens.csv file and return the decision tree using the ID3 algorithm.
-# FOR: CS 4200- Assignment #1
-# TIME SPENT: 10 min
+# SPECIFICATION: This program will output the performance of the models created based off of the test set.
+# FOR: CS 4210- Assignment #2
+# TIME SPENT: 1 hr 30 min
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
 
 #importing some Python libraries
 from sklearn import tree
-import matplotlib.pyplot as plt
 import csv
-db = []
-X = []
-Y = []
 
-#reading the data in a csv file
-with open('contact_lens.csv', 'r') as csvfile:
-  reader = csv.reader(csvfile)
-  for i, row in enumerate(reader):
-      if i > 0: #skipping the header
-         db.append (row)
-         print(row)
+dataSets = ['contact_lens_training_1.csv', 'contact_lens_training_2.csv', 'contact_lens_training_3.csv']
 
-#transform the original training features to numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
-# For the first column of entries: Young = 1, Presbyopic = 2, and Prepresbyopic = 3
-# For the second column of entries: Myope = 1, Hypermetrope = 2
-# For the third column of entries: No = 1, Yes = 2
-# For the fourth column of entries: Reduced = 1, Normal = 2
+for ds in dataSets:
 
-#--> add your Python code here
-X = [
-	[1,1,1,1],
-	[2,1,1,2],
-	[3,1,1,1],
-	[3,1,1,2],
-	[2,1,2,2],
-	[1,1,2,2],
-	[1,2,1,1],
-	[3,1,2,1],
-	[2,2,1,1],
-	[1,1,2,1]
-]
+    dbTraining = []
+    X = []
+    Y = []
 
-#transform the original training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
-# For the fifth column: No = 1, Yes = 2
-#--> add your Python code here
-Y = [1,1,1,2,2,2,1,1,1,2]
+    #reading the training data in a csv file
+    with open(ds, 'r') as csvfile:
+         reader = csv.reader(csvfile)
+         for i, row in enumerate(reader):
+             if i > 0: #skipping the header
+                dbTraining.append (row)
 
-#fitting the decision tree to the data
-clf = tree.DecisionTreeClassifier(criterion = 'entropy')
-clf = clf.fit(X, Y)
+    dict_obj = {
+        "Young": 0,
+        "Prepresbyopic": 1,
+        "Presbyopic": 2,
+        "Myope": 0,
+        "Hypermetrope": 1,
+        "Yes": 0,
+        "No": 1,
+        "Normal": 0,
+        "Reduced": 1
+    }
 
-#plotting the decision tree
-tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes','No'], filled=True, rounded=True)
-plt.show()
+    #transform the original training features to numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
+    #--> add your Python code here
+    # X =
+    for i in range(len(dbTraining)):
+        tempArr = []
+        for j in range(len(dbTraining[i])-1):
+            tempArr.append(dict_obj[dbTraining[i][j]])
+        X.append(tempArr)
+
+    #transform the original training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
+    #--> add your Python code here
+    # Y =
+    for i in range(len(dbTraining)):
+        Y.append(dict_obj[dbTraining[i][len(dbTraining[i]) - 1]])
+
+    #loop your training and test tasks 10 times here
+    acc = 100
+    for i in range (10):
+
+       #fitting the decision tree to the data setting max_depth=3
+       clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth=3)
+       clf = clf.fit(X, Y)
+
+       #read the test data and add this data to dbTest
+       #--> add your Python code here
+       dbTest = []
+       with open('contact_lens_test.csv', 'r') as csvfile:
+           reader = csv.reader(csvfile)
+           for i, row in enumerate(reader):
+               if i > 0:  # skipping the header
+                   dbTest.append(row)
+
+       count = 0
+       for data in dbTest:
+           #transform the features of the test instances to numbers following the same strategy done during training, and then use the decision tree to make the class prediction. For instance:
+           #class_predicted = clf.predict([[3, 1, 2, 1]])[0]           -> [0] is used to get an integer as the predicted class label so that you can compare it with the true label
+           #--> add your Python code here
+            for i in range(len(data)):
+                data[i] = dict_obj[data[i]]
+
+            tempArr.clear()
+
+            for i in range(len(data)-1):
+               tempArr.append(data[i])
+
+            class_predict = clf.predict([tempArr])[0]
+
+           #compare the prediction with the true label (located at data[4]) of the test instance to start calculating the accuracy.
+           #--> add your Python code here
+            if class_predict == data[4]:
+                count += 1
+
+        #find the lowest accuracy of this model during the 10 runs (training and test set)
+        #--> add your Python code here
+       if (count / len(dbTest)) < acc:
+           acc = count / len(dbTest)
+
+    #print the lowest accuracy of this model during the 10 runs (training and test set) and save it.
+    #your output should be something like that: final accuracy when training on contact_lens_training_1.csv: 0.2
+    #--> add your Python code here
+
+    print("final accuracy when training on " + ds + ": " + str(acc))
+
+
+
+
